a6bfc9c2d0666ac2c8c98579e144ebc681bf4d5a
Greshniksv <greshnik-sv@yandex.ru>
2014-06-25 18:36:09 +0300

    add file

---
+++ SrbEngine/Module.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SrbEngine
+{
+    class Module : IDisposable
+    {
+
+
+
+
+        #region Despose patternt
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Implement dispose pattern
+        public void Dispose(bool managed)
+        {
+            if (managed)
+            {
+               
+            }
+        }
+        #endregion
+    }
+}
